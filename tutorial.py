d41d61e82970dfeb3efe084f60017d692dd0bb34
Hilmi <hilmi.husaini1502@gmail.com>
2024-08-21 05:17:15 +0800

    tambah text.txt

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,7 @@
 import curses
 from curses import wrapper
 import time
+import random
 
 
 def start_screen(stdscr):
@@ -13,7 +14,6 @@ def start_screen(stdscr):
 def display_text(stdscr, target, current, wpm=0):
     stdscr.addstr(target) 
     stdscr.addstr(1,0, f"WPM: {wpm}") 
-
         
     for i, char in enumerate(current):
         correct_char = target[i]
@@ -23,8 +23,13 @@ def display_text(stdscr, target, current, wpm=0):
 
         stdscr.addstr(0, i, char, color)
 
+def load_text():
+    with open("text.txt", "r") as f:
+        lines = f.readline()
+        return random.choice(lines).strip()
+
 def wpm_test(stdscr):
-    target_text = "Hello world this is some test text for this app!"
+    target_text = load_text()
     current_text = []
     wpm = 0
     start_time = time.time()
@@ -38,10 +43,15 @@ def wpm_test(stdscr):
         display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
 
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break
+
         try:
             key = stdscr.getkey()
         except:
             continue
+
         if ord(key) == 27:
             break
 
@@ -52,13 +62,17 @@ def wpm_test(stdscr):
             current_text.append(key)
 
 
-
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
-    wpm_test(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(2, 0, "You completed the text! Press any key to continue...")
+        key = stdscr.getkey()
+        if ord(key == 27):
+            break
 
 wrapper(main) #restore prog to init state
